1eec864f49e72b04515be932cb22305f52a9b1d7
johannusone <76252121+johannusone@users.noreply.github.com>
2021-10-20 16:20:12 +0200

    Simplified color scheme

--- TemplatesandSnippets/PS_Prompt_a.ps1
+++ TemplatesandSnippets/PS_Prompt_a.ps1
@@ -5,9 +5,7 @@ function prompt {
 $promptGetLocation = Get-Location
 $promptlastCommand = Get-History -Count 1
 $promptelapsedTime = ($promptlastCommand.EndExecutionTime - $promptlastCommand.StartExecutionTime).TotalSeconds
-Write-Host "`n[$($promptelapsedTime) s] -- " -NoNewLine -ForegroundColor DarkYellow
-
-Write-Host "$promptGetLocation\" -ForegroundColor DarkGreen
+Write-Host "`n[$($promptelapsedTime) s] -- $promptGetLocation\" -ForegroundColor DarkGreen
 Write-Host "PS - $($promptGetLocation | Split-Path -Leaf)\>" -NoNewline -ForegroundColor Gray
 return " "
 }
