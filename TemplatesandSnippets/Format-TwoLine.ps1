aa758120be0b5d5fea95956e907c9dabaea02fd9
johannusone <sandoz@freemail.hu>
2021-11-13 03:42:04 +0100

    Removed unnecessary code and format

--- TemplatesandSnippets/Format-TwoLine.ps1
+++ TemplatesandSnippets/Format-TwoLine.ps1
@@ -3,18 +3,17 @@
 function Format-TwoLine {
 
     param (
-    [System.ConsoleColor]$BGColor1 = 'DarkBlue',
-    [System.ConsoleColor]$BGColor2 = 'DarkBlue',
-    [System.ConsoleColor]$FGColor1 = 'DarkYellow',
-    [System.ConsoleColor]$FGColor2 = 'DarkCyan'
+        [System.ConsoleColor]$BGColor1 = 'DarkBlue',
+        [System.ConsoleColor]$BGColor2 = 'DarkBlue',
+        [System.ConsoleColor]$FGColor1 = 'DarkYellow',
+        [System.ConsoleColor]$FGColor2 = 'DarkCyan'
     )
 
     $TwoLineInner = {
-        begin {$ww = $false; $index = 0}
+        begin {$ww = $false}
         process {
             Write-Host $_ -BackgroundColor $(if ($ww) {$BGColor1} else {$BGColor2}) -ForegroundColor $(if ($ww) {$FGColor1} else {$FGColor2})
             $ww = -not $ww
-            $index++
         }
     }
     $input | Out-String -Stream | & $TwoLineInner
