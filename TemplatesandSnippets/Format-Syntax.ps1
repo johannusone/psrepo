6ec09e108f7c62becaa04590c185b2ff34936661
johannusone <sandoz@freemail.hu>
2024-12-08 16:31:17 +0100

    Added prompt

--- TemplatesandSnippets/Format-Syntax.ps1
+++
@@ -1,3 +0,0 @@
-# Formats the output of Get-Command -syntax with line breaks
-function Format-Syntax { $input -replace "(\s\[+-|\s-|\s\[<)", "`n `$1" }
-Set-Alias -Name fs -Value Format-Syntax
--- TemplatesandSnippets/Template_for_ScheduledTask.ps1
+++ TemplatesandSnippets/Template_for_ScheduledTask.ps1
@@ -1,5 +1,3 @@
-Throw "Make sure the below script is what you want to run.";
-
 $action = New-ScheduledTaskAction -Execute 'PowerShell.exe' -argument "full_path_of_the_ps1_file"
 $trigger = New-ScheduledTaskTrigger -Daily -At 18:00
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "_name_of_the_task" -Description "Description_text"
